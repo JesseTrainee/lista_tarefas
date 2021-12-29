de0c8d71ea432d9058a31b5a120f6b258110aeea
JesseTrainee <a.l.i.t.t.l.e@hotmail.com>
2021-12-29 11:47:44 -0300

    changing styles and adding background

--- app_lista_tarefas_public/index.php
+++ app_lista_tarefas_public/index.php
@@ -7,11 +7,13 @@
 	<head>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>App Lista Tarefas</title>
-
-		<link rel="stylesheet" href="css/estilo.css">
+		<title>App Task List</title>
+		
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400&display=swap" rel="stylesheet">
+		<link rel="stylesheet" href="./css/estilo.css">
 		<script>
 			function editar(id, txt_tarefa) {
 
@@ -56,44 +58,47 @@
 	</head>
 
 	<body>
-		<nav class="navbar navbar-light bg-light">
-			<div class="container">
-				<a class="navbar-brand" href="#">
-					<img src="img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
-					App Lista Tarefas
-				</a>
-			</div>
-		</nav>
-
-		<div class="container app">
-			<div class="row">
-				<div class="col-md-3 menu">
-					<ul class="list-group">
-						<li class="list-group-item active"><a href="#">Tarefas pendentes</a></li>
-						<li class="list-group-item"><a href="nova_tarefa.php">Nova tarefa</a></li>
-						<li class="list-group-item"><a href="todas_tarefas.php">Todas tarefas</a></li>
-					</ul>
+		<div id="context">
+			<nav class="navbar navbar-light">
+				<div class="container">
+					<a class="navbar-brand" href="#">
+						<img src="img/check-list-icon.png" id="icon" class="d-inline-block align-top" alt="">
+						<span id="title">App Task List</span>
+					</a>
 				</div>
+			</nav>
 
-				<div class="col-md-9">
-					<div class="container pagina">
-						<div class="row">
-							<div class="col">
-								<h4>Tarefas pendentes</h4>
-								<hr />
-								<?php foreach( $tarefas as $indice => $tarefa ) { ?>
-									<div class="row mb-3 d-flex align-items-center tarefa">
-										<div class="col-sm-9" id="tarefa_<?= $tarefa->id ?>">
-											<?= $tarefa->tarefa ?>
-										</div>
-										<div class="col-sm-3 mt-2 d-flex justify-content-between">
-											<i class="fas fa-trash-alt fa-lg text-danger" onclick="remover(<?= $tarefa->id?>)"></i>
-											<i class="fas fa-edit fa-lg text-info" onclick="editar( <?= $tarefa->id ?>, '<?= $tarefa->tarefa ?>' )"></i>
-											<i class="far fa-square fa-lg" onclick="marcarRealizada(<?= $tarefa->id ?>)"></i>
-										</div>
-									</div>
-								<?php } ?>
+			<div class="container app">
+				<div class="row">
+					<div class="col-md-3 menu">
+						<ul class="list-group">
+							<li class="list-group-item active"><a href="#">Tarefas pendentes</a></li>
+							<li class="list-group-item"><a href="nova_tarefa.php">Nova tarefa</a></li>
+							<li class="list-group-item"><a href="todas_tarefas.php">Todas tarefas</a></li>
+						</ul>
+					</div>
 
+					<div class="col-md-9">
+						<div class="container pagina">
+							<div class="row">
+								<div class="col">
+									<h4>Tarefas pendentes</h4>
+									<hr />
+									<?php foreach( $tarefas as $indice => $tarefa ) { ?>
+										<div class="row mb-3 d-flex align-items-center tarefa">
+											<div class="col-sm-9" id="tarefa_<?= $tarefa->id ?>">
+												<i class="far fa-square fa-lg pr-2" onclick="marcarRealizada(<?= $tarefa->id ?>)"></i>
+												<?= $tarefa->tarefa ?>
+											</div>
+											<div class="col-sm-3 mt-2 d-flex">
+												<i class="fas fa-trash-alt fa-lg text-danger" onclick="remover(<?= $tarefa->id?>)"></i>
+												<i class="fas fa-edit fa-lg text-info pl-5" onclick="editar( <?= $tarefa->id ?>, '<?= $tarefa->tarefa ?>' )"></i>
+											</div>
+										</div>
+										<hr class="dashed-separator">
+									<?php } ?>
+
+								</div>
 							</div>
 						</div>
 					</div>
